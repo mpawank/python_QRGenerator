a189de0c1b4b6cc1fe9fe243d0fff00669befb15
M PAWAN KUMAR <146627172+mpawank@users.noreply.github.com>
2024-03-18 01:12:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello World')
+import qrcode
+from PIL import Image
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=20,border=4,)
+qr.add_data("https://github.com/mpawank")
+qr.make(fit=True)
+img=qr.make_image()
+img.save("mygithub.png")
